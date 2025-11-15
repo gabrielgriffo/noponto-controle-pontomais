bc71fc8ccfd8b4e39e50fbb1e53d0d644c0054ee
Gabriel Griffo <gabriel_griffo@hotmail.com>
2025-11-15 10:06:12 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+
+const path = require('node:path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+
+  win.once('ready-to-show', () => {
+    win.show();
+  });
+}
+
+app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
+  
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
