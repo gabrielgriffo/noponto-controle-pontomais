871b456fd506c37d02fc6975379f1052fe5239ad
Gabriel Griffo <gabriel_griffo@hotmail.com>
2025-12-22 22:47:38 -0300

    style: ajusta estilos de interface e desabilita menu de contexto

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { bootstrapApplication } from "@angular/platform-browser";
 import { App } from "./app/app";
 import { appConfig } from "./app/app.config";
 
+document.addEventListener('contextmenu', (event) => {
+  event.preventDefault();
+  return false;
+});
+
 bootstrapApplication(App, appConfig).catch((err) =>
   console.error(err),
 );
